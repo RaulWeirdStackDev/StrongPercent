1cd3b3137e0966cc046612e2677275682efb75e2
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-05-10 22:24:02 -0400

    eliminar eventos

--- pesas.js
+++ pesas.js
@@ -5,29 +5,28 @@ const calcular = document.querySelector("#calcular");
 const body = document.querySelector("body");
 const navLinks= document.querySelectorAll("a")
 
-const calcularPorcentaje= () => calcular.addEventListener("click", () => {
+const calcularPorcentaje= () => {
     const total = (peso.value * porcentje.value) / 100;
     resultado.textContent = "El peso es de: " + total + "kg";
-});
+};
 
-const libras= () => calcular.addEventListener("click", () => {
+const libras= () =>  {
     const total = (peso.value * 0.453592) ;
     resultado.textContent = "El peso es de: " + total + "kg";
-});
+};
 
 const theme = () => {
-    body.addEventListener("click", () => {
-        const currentBg = getComputedStyle(body).backgroundColor;
+    const currentBg = getComputedStyle(body).backgroundColor;
 
-        if (currentBg === "rgb(20, 18, 18)") {
-            body.style.backgroundColor = "white";
-            body.style.color = "black";
-            navLinks.forEach(link => link.style.color = "black");
-        } else {
-            body.style.backgroundColor = "#141212";
-            body.style.color = "white";
-            navLinks.forEach(link => link.style.color = "white");
-        }
-    });
+    if (currentBg === "rgb(20, 18, 18)") {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        document.querySelectorAll("a").forEach(a => a.style.color = "black");
+    } else {
+        body.style.backgroundColor = "#141212";
+        body.style.color = "white";
+        document.querySelectorAll("a").forEach(a => a.style.color = "white");
+    }
 };
+
 
