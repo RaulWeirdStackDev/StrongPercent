32e8c1aec5774a8db4571520caa9140a4d345b35
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-05-10 22:43:14 -0400

    botón de instalación

---
+++ sw.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('strongpercent').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/kilos.html',
+          '/pesas.js',
+          '/style.css',
+         '/logo.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['strongpercent'];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
