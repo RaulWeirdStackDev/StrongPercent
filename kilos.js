e09909403d78e63b834466969df906a266bfe52f
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-05-10 22:02:14 -0400

    dark and light theme

--- kilos.js
+++
@@ -1,8 +0,0 @@
-const peso = document.querySelector("#peso");
-const resultado = document.querySelector("#resultado");
-const calcular = document.querySelector("#calcular");
-
-calcular.addEventListener("click", () => {
-    const total = (peso.value * 0.453592) ;
-    resultado.textContent = "El peso es de: " + total + "kg";
-});
--- pesas.js
+++ pesas.js
@@ -2,8 +2,29 @@ const peso = document.querySelector("#peso");
 const porcentje = document.querySelector("#porcentaje");
 const resultado = document.querySelector("#resultado");
 const calcular = document.querySelector("#calcular");
+const body = document.querySelector("body");
 
-calcular.addEventListener("click", () => {
+const calcularPorcentaje= () => calcular.addEventListener("click", () => {
     const total = (peso.value * porcentje.value) / 100;
     resultado.textContent = "El peso es de: " + total + "kg";
 });
+
+const libras= () => calcular.addEventListener("click", () => {
+    const total = (peso.value * 0.453592) ;
+    resultado.textContent = "El peso es de: " + total + "kg";
+});
+
+const theme = () => {
+    body.addEventListener("click", () => {
+        const currentBg = getComputedStyle(body).backgroundColor;
+
+        if (currentBg === "rgb(20, 18, 18)") {
+            body.style.backgroundColor = "white";
+            body.style.color = "black";
+        } else {
+            body.style.backgroundColor = "#141212";
+            body.style.color = "white";
+        }
+    });
+};
+
